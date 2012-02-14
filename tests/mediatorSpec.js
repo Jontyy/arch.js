3557f48378ff5012a569e70d038cd09fd11cd877
John White <jonttyy@gmail.com>
2012-02-14 20:45:19 +0000

    validation on the publish method

--- tests/mediatorSpec.js
+++ tests/mediatorSpec.js
@@ -16,43 +16,57 @@ describe('arch.mediator',function(){
 	describe('pubsub',function(){
 		var id = null, spy = jasmine.createSpy(function(){});	
 
-		it('Should only accept string+function parameters',function(){
-			expect( function(){
-				arch.mediator.subscribe({});
-			} ).toThrow(new Error('Event name must be a string.'));
+		describe('Subscribe',function(){
+			it('Should only accept string+function parameters',function(){
+				expect( function(){
+					arch.mediator.subscribe({});
+				} ).toThrow(new Error('Event name must be a string.'));
 
-			expect(function(){
-				arch.mediator.subscribe('eventName',{});
-			}).toThrow(new Error('Callback must be a function.'))
+				expect(function(){
+					arch.mediator.subscribe('eventName',{});
+				}).toThrow(new Error('Callback must be a function.'));
+
+			});
+
+			it('Should allow to subscribe to events + return event id',function(){
+				var i = arch.mediator.subscribe('myevent',spy);
+				expect(typeof i).toBe('number');
+				id = i;
+			});
+
+			it('Should provide a different id for each subscription',function(){
+				var i = arch.mediator.subscribe('eventName',function(){});
+				expect(i).toNotEqual(id);
+				expect(i).toBeGreaterThan(id);	
+			});
+		});
+
+		describe('Publish',function(){
+			
+			it('Should only accept string for event name',function(){
+				expect(function(){
+					arch.mediator.publish({});
+				}).toThrow(new Error('Event name must be a string.'));
+			});
+
+			it('Should publish',function(){
+				arch.mediator.publish('myevent','test');
+				expect(spy).toHaveBeenCalledWith('test');
+			});
+
+			it('Should only call the correct callbacks',function(){
+				arch.mediator.publish('monkey','monkey');
+				expect(spy).wasNotCalledWith('monkey');	
+			});
 
 		});
 
-		it('Should allow to subscribe to events + return event id',function(){
-			var i = arch.mediator.subscribe('myevent',spy);
-			expect(typeof i).toBe('number');
-			id = i;
-		});
-
-		it('Should provide a different id for each subscription',function(){
-			var i = arch.mediator.subscribe('eventName',function(){});
-			expect(i).toNotEqual(id);
-			expect(i).toBeGreaterThan(id);	
-		});
-
-		it('Should publish',function(){
-			arch.mediator.publish('myevent','test');
-			expect(spy).toHaveBeenCalledWith('test');
-		});
-
-		it('Should only call the correct callbacks',function(){
-			arch.mediator.publish('monkey','monkey');
-			expect(spy).wasNotCalledWith('monkey');	
-		});
-
-		it('Should allow unsubscribe',function(){
-			arch.mediator.unsubscribe(id);
-			arch.mediator.publish('myevent','unsubscribed');
-			expect(spy).wasNotCalledWith('unsubscribed');
+		describe('unsubscribe',function(){
+			it('Should allow unsubscribe',function(){
+				arch.mediator.unsubscribe(id);
+				arch.mediator.publish('myevent','unsubscribed');
+				expect(spy).wasNotCalledWith('unsubscribed');
+			});
 		});
 	});
 
