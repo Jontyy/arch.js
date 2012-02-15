f9d4199e718937db554782d12d70c1fa4d497600
John White <jonttyy@gmail.com>
2012-02-15 21:48:51 +0000

    formatting module.js

--- src/module.js
+++ src/module.js
@@ -1,47 +1,49 @@
-(function(arch){
+(function(arch) {
 	"use strict";
 	var module = {},
 
-	modules = {},
+		modules = {},
 
-	constructors = {},
+		constructors = {},
 
-	//shortcut for throwng an error
-		error = function(message){
+		//shortcut for throwng an error
+		error = function(message) {
 			throw new Error(message);
 		},
 
-	//method for actually starting a module	
-	startModule = function(name){
+		//method for actually starting a module	
+		startModule = function(name) {
 			var m = constructors[name],
 				elem = document.getElementById(name);
 
 			m = m.call(elem, new arch.Sandbox(elem));
-			if(typeof m !== 'object' || typeof m.init !== 'function' || typeof m.destroy !== 'function'){
+			if (typeof m !== 'object' || typeof m.init !== 'function' || typeof m.destroy !== 'function') {
 				error('Module constructor should return an object with init and destroy methods.');
 			}
 			m.init();
-			modules[name] = m;	
-	};
+			modules[name] = m;
+		};
 
-	module.register = function(/*string*/ name, /*function*/ constructor){
+	module.register = function( /*string*/ name, /*function*/ constructor) {
 		typeof name !== 'string' && error('Module name must be a string.');
 		typeof constructor !== 'function' && error('Module constructor must be a function.');
 		constructors[name] = constructor;
 	};
-	module.start = function(/*string*/ name){
-		var i,m,elem;
-		for(i in arguments){if(arguments.hasOwnProperty(i)){
-			typeof arguments[i] !== 'string' && error('Module names must be strings.');
-			typeof constructors[arguments[i]] !== 'function' && error('Module not found.');
-			startModule(arguments[i]);
-		}}
+	module.start = function( /*string*/ name) {
+		var i, m, elem;
+		for (i in arguments) {
+			if (arguments.hasOwnProperty(i)) {
+				typeof arguments[i] !== 'string' && error('Module names must be strings.');
+				typeof constructors[arguments[i]] !== 'function' && error('Module not found.');
+				startModule(arguments[i]);
+			}
+		}
 	};
-	module.stop = function(/*string*/ name){};
-	module.startAll = function(){
+	module.stop = function( /*string*/ name) {};
+	module.startAll = function() {
 
 	};
-	module.stopAll = function(){};
+	module.stopAll = function() {};
 
 	arch.module = module;
 }(window.arch));
